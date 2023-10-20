147ed049224c7f42ac3d3968d75f4478472cda5c
zeeshanhanif <zeeshanhanif@gmail.com>
2023-10-20 23:41:42 +0500

    Updated Nusic Metadata contract

--- contracts/NusicMetadata.sol
+++ contracts/NusicMetadata.sol
@@ -10,9 +10,6 @@ import "hardhat/console.sol";
 contract NusicMetadata is Ownable, ReentrancyGuard {
     using Strings for uint256;
 
-    string public artistName;
-    string public artistId;
-    address public artistAddress;
     uint256 public metadataIndex;
 
     address public manager;
@@ -27,34 +24,28 @@ contract NusicMetadata is Ownable, ReentrancyGuard {
                             // Edition will not have token id, Lazy will not have token id and contract address
     }
 
-    mapping(uint256 => Metadata) public usersMetadata;
+    mapping(string => Metadata) public usersMetadata;
 
     event PublishMetadata(uint256 index, uint256 metadataType, address contributorAddress, address nftAddress, uint256 tokenId,  string metadataFileReferenceId);
-    event UpdateMetadata(uint256 index, string newMetadataFileReferenceId);
+    event UpdateMetadata(address nftAddress, uint256 tokenId, string newMetadataFileReferenceId);
 
-    modifier onlyArtist() {
-        require((artistAddress == msg.sender), "Caller needs to be Artist of Contract");
+
+    modifier onlyOwnerOrManager() {
+        require((owner()== msg.sender) || (manager == msg.sender), "Caller needs to be Owner or Manager");
         _;
     }
 
-    modifier onlyArtistOrManager() {
-        require((artistAddress== msg.sender) || (manager == msg.sender), "Caller needs to be Owner or Manager");
-        _;
+    constructor() {
     }
 
-    constructor(string memory _artistName, string memory _artistId, address _artistAddress) {
-        require(_artistAddress != address(0), "Null Address Provided");
-        require(bytes(_artistName).length > 0, "Artist name cannot be empty");
-        require(bytes(_artistId).length > 0, "Artist Id cannot be empty");
-        artistName = _artistName;
-        artistId = _artistId;
-        artistAddress = _artistAddress;
-    }
-
-    function publishMetadata(string memory _metadataFileReferenceId, address _nftAddress, uint256 _tokenId, uint256 _metadataType) internal onlyArtist nonReentrant {
+    function publishMetadata(string memory _metadataFileReferenceId, address _nftAddress, uint256 _tokenId, uint256 _metadataType) internal onlyOwnerOrManager {
         require(bytes(_metadataFileReferenceId).length > 0, "Meatadata file CID cannot be empty");
         require(_nftAddress != address(0), "Null Address Provided");
-        Metadata storage data = usersMetadata[metadataIndex];
+        string memory uniqueId = string(abi.encodePacked(_nftAddress, _tokenId));
+        Metadata storage data = usersMetadata[uniqueId];
+
+        require(data.nftAddress == address(0), "Metadata already exists");
+
         data.index = metadataIndex;
         data.contributorAddress = msg.sender;
         data.nftAddress = _nftAddress;
@@ -65,37 +56,44 @@ contract NusicMetadata is Ownable, ReentrancyGuard {
         metadataIndex++;
     }
 
-    function publishCollectionMetadata(string memory _metadataFileReferenceId, address _nftAddress, uint256 _tokenId) public onlyArtist nonReentrant {
-        publishMetadata(_metadataFileReferenceId, _nftAddress, _tokenId, 1);
+    function publishCollectionMetadata(string memory _metadataFileReferenceId, address _nftAddress) public nonReentrant {
+        publishMetadata(_metadataFileReferenceId, _nftAddress, 0, 1);
     }
 
-    function publishEditionMetadata(string memory _metadataFileReferenceId, address _nftAddress) public onlyArtist nonReentrant {
-        publishMetadata(_metadataFileReferenceId, _nftAddress, 0, 2);
+    function publishEditionMetadata(string memory _metadataFileReferenceId, address _nftAddress, uint256 _tokenId) public nonReentrant {
+        publishMetadata(_metadataFileReferenceId, _nftAddress, _tokenId, 2);
     }
 
-    function publishLazyMetadata(string memory _metadataFileReferenceId) public onlyArtist nonReentrant {
+    function publishLazyMetadata(string memory _metadataFileReferenceId) public nonReentrant {
         publishMetadata(_metadataFileReferenceId, address(0), 0, 3);
     }
 
-    function updateMetadata(uint256 _metadataIndex, string memory _newMetadataFileReferenceId) public onlyArtist nonReentrant {
+    function updateMetadata(address _nftAddress, uint256 _tokenId, string memory _newMetadataFileReferenceId) public onlyOwnerOrManager nonReentrant {
         require(bytes(_newMetadataFileReferenceId).length > 0, "Meatadata file CID cannot be empty");
-        Metadata storage data = usersMetadata[_metadataIndex];
+        string memory uniqueId = string(abi.encodePacked(_nftAddress, _tokenId));
+        Metadata storage data = usersMetadata[uniqueId];
         require(data.nftAddress != address(0), "Metadata not found");
         data.metadataFileReferenceId.push(_newMetadataFileReferenceId);
-        emit UpdateMetadata(_metadataIndex, _newMetadataFileReferenceId);
+        emit UpdateMetadata(_nftAddress, _tokenId, _newMetadataFileReferenceId);
     }
 
-    function getMetadata(uint256 _metadataIndex) public view returns(Metadata memory) {
-        return usersMetadata[_metadataIndex];
+    function getMetadata(address _nftAddress, uint256 _tokenId) public view returns(Metadata memory) {
+        string memory uniqueId = string(abi.encodePacked(_nftAddress, _tokenId));
+        return usersMetadata[uniqueId];
     }
 
+    function metadataCount() public view returns(uint256) {
+        return metadataIndex;
+    }
+    /*
     function getAllMetadata() public view returns(Metadata[] memory _metadata){
         for (uint256 i = 0; i < metadataIndex; i++) {
             _metadata[i]=(usersMetadata[i]);
         }
     }
+    */
 
-    function setManager(address _manager) public onlyArtist {
+    function setManager(address _manager) public onlyOwner {
         manager = _manager;
     }
 
